833400f996cec68c22dfa7862c318b89069bd13c
Ryu Jae-kuk <jakuky@hanmail.net>
2023-09-04 00:43:37 +0900

    Add files via upload
    수익화 추가

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # env 파일을 사용하기 위한 라이브러리 import
-#from dotenv import load_dotenv
-#load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 
 # langchain, chat 모드 사용   
 from langchain.chat_models import ChatOpenAI
@@ -17,20 +17,30 @@ chat_model = ChatOpenAI()
 import streamlit as st
 from PIL import Image
 
+# 화면 상단 여백 제거
 st.write("<style>div.block-container{padding-top:2rem;}</style>", unsafe_allow_html=True)
 
-st.markdown("## AI & Poem")
+# 웹페이지 제목
+st.title("AI & Poem")
 st.markdown("### Your Topics, Our AI Poems")
 
+# 그림 삽입
 col1, col2, col3 = st.columns([0.25, 0.5, 0.25])
 with col2:
     img = Image.open("AI-Poet.png")
     st.image(img)
 
+# 시 주제 입력 받음
 content = st.text_input("시의 주제를 제시해 주세요")
 
+# 시 작성
 if st.button('시 작성 요청'):
     with st.spinner('시 작성 중... 잠시만 기다려 주세요'):
         result  = chat_model.predict(content + "에 대한 시를 써주세요")
         st.write(result)
+        
+# 수익화
+from streamlit_extras.buy_me_a_coffee import button
+button(username="jakukyr", floating=True, width=221) 
+
 
