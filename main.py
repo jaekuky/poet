539b1d8e7cbd56cc41661a6bf1adbfe84be02994
Ryu Jae-kuk <jakuky@hanmail.net>
2023-09-06 20:13:53 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # env 파일을 사용하기 위한 라이브러리 import
-#from dotenv import load_dotenv
-#load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 # langchain, chat 모드 사용   
 from langchain.chat_models import ChatOpenAI
@@ -25,22 +25,31 @@ st.title("AI & Poem")
 st.markdown("### Your Topics, Our AI Poems")
 
 # 그림 삽입
-col1, col2, col3 = st.columns([0.25, 0.5, 0.25])
-with col2:
+pictureCols = []
+pictureCols = st.columns([0.25, 0.5, 0.25])
+with pictureCols[1]:
     img = Image.open("AI-Poet.png")
     st.image(img)
 
 # 시 주제 입력 받음
 content = st.text_input("시의 주제를 제시해 주세요")
+    
+# 버튼 만들기
+# 시 작성 & 시 낭송
+from gtts import gTTS
+if st.button("시 작성 요청"):
+    with st.spinner("시 작성 중... 잠시만 기다려 주세요"):
+        # 시 작성
+        poem  = chat_model.predict(content + "에 대한 시를 써주세요")
+        st.write(poem)
 
-# 시 작성
-if st.button('시 작성 요청'):
-    with st.spinner('시 작성 중... 잠시만 기다려 주세요'):
-        result  = chat_model.predict(content + "에 대한 시를 써주세요")
-        st.write(result)
-        
+        # Google TTS를 사용하여 시 낭송
+        tts = gTTS(text=poem, lang="ko", slow=False)
+        tts.save("poem.mp3")
+        st.audio("poem.mp3")
+   
 # 수익화
 from streamlit_extras.buy_me_a_coffee import button
-button(username="jakukyr", floating=True, width=221) 
+button(username="jakukyr", floating=False, width=221) 
 
 
