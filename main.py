c8c96c39f5237fa8202cfd22d91b27a3d45c49a0
Ryu Jae-kuk <jakuky@hanmail.net>
2023-09-14 10:44:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -51,6 +51,6 @@ if st.button("시 작성 요청"):
    
 # 수익화
 from streamlit_extras.buy_me_a_coffee import button
-button(username="jakukyr", floating=True, font="Lato", width=221) 
+button(username="jakukyr", floating=True, font="Lato", width=260) 
 
 st.write("Powered by ChatGPT and LangChain")
